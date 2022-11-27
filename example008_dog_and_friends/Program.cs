487d0e72d60a5d5b3823766799766053d72a18d2
Ramazan <coldalbard.@gmail.com>
2022-11-28 01:08:17 +0300

    added example008_dog_and_friends

---
+++ example008_dog_and_friends/Program.cs
@@ -0,0 +1,38 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int Time = 0;
+
+if(distance < 10)
+{
+    Console.Write(count);
+}
+else
+{
+    while(distance)
+    {
+        if(distance > 10)
+        {
+
+            if(friend == 1)
+            {
+                Time = distance / (firstFriendSpeed + dogSpeed);
+                friend = 2;
+                distance = distance - (firstFriendSpeed + SecondFriendSpeed);
+                count= count + 1;
+        
+            }
+            if(friend != 1)
+            {
+                Time = distance / (SecondFriendSpeed + dogSpeed);
+                friend = 1;
+                distance = distance - (firstFriendSpeed + SecondFriendSpeed);
+                count = count + 1;
+            }
+        }
+    Console.WriteLine(count);
+    }
+}
