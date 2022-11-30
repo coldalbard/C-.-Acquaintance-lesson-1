3eead960b8de81ac80177dd9c5a5850ccc818cd6
Ramazan <coldalbard.@gmail.com>
2022-11-30 22:00:36 +0300

    added new file

---
+++ example010_intro_array/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {11, 21, 31, 14, 15, 61, 17, 18, 91};
+array[0] = 12;
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
+// int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
